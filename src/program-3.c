911726d2416135d21576a96b681816300f91c1b8
Aabish Malik <aabishmalik.dev@proton.me>
2024-03-23 16:00:15 +0000

    Added program 4 and relevant screenshots

--- src/program-3.c
+++ src/program-3.c
@@ -6,45 +6,42 @@
 
 int main()
 {
-	char BUFFER[128], STK[128];
-	printf("Enter expression: ");
-	scanf("\n%[^\n]127s", BUFFER);
-	int len = strlen(BUFFER);
-	int top = -1;
-	char is_valid = 1;
+		char BUFFER[128], STK[128];
+		printf("Enter expression: ");
+		scanf("\n%[^\n]127s", BUFFER);
+		int len = strlen(BUFFER);
+		int top = -1;
+		char is_valid = 1;
 
-	for (int i = 0; i < len && is_valid; i++) {
-		switch (BUFFER[i]) {
-		case '(':
-		case '{':
-		case '[':
-			STK[++top] = BUFFER[i];
-			break;
-		case ')':
-			if (STK[top] == '(') {
-				top--;
-			} else
-				is_valid = 0;
-			break;
-		case ']':
-			if (STK[top] == '[') {
-				top--;
-			} else
-				is_valid = 0;
-			break;
-		case '}':
-			if (STK[top] == '{') {
-				top--;
-			} else
-				is_valid = 0;
-			break;
-		default:
-			break;
+		for (int i = 0; i < len && is_valid; i++) {
+				switch (BUFFER[i]) {
+				case '(':
+				case '{':
+				case '[': STK[++top] = BUFFER[i]; break;
+				case ')':
+						if (STK[top] == '(') {
+								top--;
+						} else
+								is_valid = 0;
+						break;
+				case ']':
+						if (STK[top] == '[') {
+								top--;
+						} else
+								is_valid = 0;
+						break;
+				case '}':
+						if (STK[top] == '{') {
+								top--;
+						} else
+								is_valid = 0;
+						break;
+				default: break;
+				}
 		}
-	}
 
-	if (is_valid)
-		printf("Valid expression\n");
-	else
-		printf("Invalid expression\n");
+		if (is_valid)
+				printf("Valid expression\n");
+		else
+				printf("Invalid expression\n");
 }
