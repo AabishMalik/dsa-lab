39d298931d09ce592f1067534bce466166f924c5
Aabish Malik <aabishmalik.dev@proton.me>
2024-05-16 17:54:03 +0000

    add p11 - p13

--- src/program-8.c
+++ src/program-8.c
@@ -11,86 +11,81 @@ typedef struct Node {
 	struct Node *prev;
 } node_t;
 
-node_t *head = NULL;
-node_t *tail = NULL;
+node_t *head = NULL, *tail = NULL;
 size_t length = 0;
 
-void Insert(size_t idx, int data){
+void Insert(size_t idx, int data) {
+	if (idx > length) return;
 	node_t *node = (node_t *)malloc(sizeof(node_t));
 	node->data = data;
-	node->next = node->prev = NULL;
 
-	if(length == 0){
+	if (length == 0) {
 		head = tail = node;
-	}else if(idx == 0){
+		node->next = node->prev = NULL;
+	} else if (idx == 0) {
 		node->next = head;
+		node->prev = NULL;
 		head->prev = node;
 		head = node;
-	}else{
+	} else {
 		node_t *current = head;
-		for(size_t i=0; i<idx-1 && current->next != NULL; i++){
+		for (size_t i = 0; i < idx - 1 && current->next != NULL; i++) {
 			current = current->next;
 		}
 		node->next = current->next;
 		node->prev = current;
 		current->next = node;
-		if(node->next == NULL) tail = node;
-		else{
+		if (node->next == NULL)
+			tail = node;
+		else {
 			node->next->prev = node;
 		}
 	}
 	length++;
 }
 
-void Search(int data){
+void Search(int data) {
 	node_t *current = head;
-	int idx = 0;
-	while(current != NULL){
-		if(current->data == data){
+
+	for (int idx = 0; current != NULL; current = current->next, idx++) {
+		if (current->data == data) {
 			printf("Found %d at index %d\n", data, idx);
 			return;
 		}
-		current = current->next;
-		idx++;
 	}
 	printf("%d not found\n", data);
 }
 
-void Display(){
-	node_t *current = head;
-	while(current != NULL){
+void Display() {
+	for (node_t *current = head; current != NULL; current = current->next) {
 		printf("%d%s", current->data, (current->next != NULL) ? "<->" : "\n");
-		current = current->next;
 	}
 }
 
-void Delete(size_t idx){
-	if(length == 0) return;
+void Delete(size_t idx) {
+	if (length == 0 || idx > length) return;
+
 	node_t *current = head;
-	if(idx == 0){
+	if (idx == 0) {
 		head = head->next;
-		head->prev = NULL;
-	}else if (idx == length-1){
+	} else if (idx == length - 1) {
+		current = tail;
 		tail = tail->prev;
 		tail->next = NULL;
-	}else if(idx >= length){
-		printf("Index out of bounds\n");
-		return;
-	}else{
-		for(size_t i = 0; i<idx; i++){
-			current = current->next;
-		}
-		if(current != NULL){
-			current->prev->next = current->next;
-			current->next->prev = current->prev;
-		}
+	} else {
+		for (size_t i = 0; i < idx; i++) current = current->next;
+		current->prev->next = current->next;
+		current->next->prev = current->prev;
 	}
 	free(current);
 	length--;
 }
 
-int main(){
-	Insert(0, 1); Insert(1, 9); Insert(3, 10); Insert(1, 5);
+int main() {
+	Insert(0, 1);
+	Insert(1, 9);
+	Insert(3, 10);
+	Insert(1, 5);
 	Display();
 	Search(5);
 	Search(11);
--- src/program-9.c
+++ src/program-9.c
@@ -2,7 +2,6 @@
 //
 // Basic implementation of a circular linked list
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,9 +14,9 @@ node_t *head = NULL, *tail = NULL;
 size_t length = 0;
 
 void Insert(size_t idx, int data) {
+	if (idx > length) return;
 	node_t *node = (node_t *)malloc(sizeof(node_t));
 	node->data = data;
-	node->next = NULL;
 
 	if (length == 0) {
 		head = tail = node;
@@ -26,15 +25,13 @@ void Insert(size_t idx, int data) {
 		node->next = head;
 		head = node;
 		tail->next = head;
-	} else if (idx >= length) {
+	} else if (idx == length) {
 		tail->next = node;
 		tail = node;
 		tail->next = head;
 	} else {
 		node_t *current = head;
-		for (size_t i = 0; i < idx - 1; i++) {
-			current = current->next;
-		}
+		for (size_t i = 0; i < idx - 1; i++) current = current->next;
 		node->next = current->next;
 		current->next = node;
 	}
@@ -43,16 +40,11 @@ void Insert(size_t idx, int data) {
 
 void Search(int data) {
 	node_t *current = head;
-	int idx = 0;
-	while (current != NULL) {
+	for (int idx = 0; current != NULL && current->next != head;
+	     current = current->next, idx++) {
 		if (current->data == data) {
 			printf("Found %d at index %d\n", data, idx);
 			return;
-		}
-		current = current->next;
-		idx++;
-		if (current == head) {
-			break;
 		}
 	}
 	printf("%d not found\n", data);
@@ -60,32 +52,32 @@ void Search(int data) {
 
 void Display() {
 	node_t *current = head;
-	while (current != NULL) {
+	do {
 		printf("%d%s", current->data, (current->next != head) ? "->" : "\n");
 		current = current->next;
-		if (current == head) {
-			break;
-		}
-	}
+	} while (current != head);
 }
 
 void Delete(size_t idx) {
 	if (length == 0) return;
 	node_t *current = head;
 	if (idx == 0) {
-		head = head->next;
-		tail->next = head;
-	} else {
-		for (size_t i = 0; i < idx - 1; i++) {
-			current = current->next;
+		if (length == 1) {
+			head = tail = NULL;
+		} else {
+			head = head->next;
+			tail->next = head;
 		}
+		free(current);
+
+	} else {
+		for (size_t i = 0; i < idx - 1; i++) current = current->next;
 		node_t *temp = current->next;
 		current->next = temp->next;
 		free(temp);
 	}
 	length--;
 }
-
 
 int main() {
 	Insert(0, 1);
